da98987439499f2615e3270a6d26fd3470c4842f
wombat <u58_makcza_waw@technischools.com>
2025-01-08 00:41:38 +0100

    rotation of misery

--- player.gd
+++ player.gd
@@ -3,37 +3,17 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const LOOKAROUND_SPEED = 0.01
+const LOOKAROUND_SPEED = 1
 
-var raw_ijk := Vector3(1, 0, 0)
-var ijk: Vector3
-var raw_w : float
-var quat: Quaternion
-var final_rotation: Quaternion
-var is_rotating := false
-
-func update_quat():
-	if raw_ijk.length() > 0:
-		ijk = raw_ijk.normalized()
-	quat = Quaternion(ijk, deg_to_rad(raw_w))
-	quaternion = Quaternion(Vector3.FORWARD, ijk)
-
-func _on_rotate_pressed():
-	if is_rotating:
-		quaternion = final_rotation
-	is_rotating = true
-	final_rotation = (quat * quaternion).normalized()
+var goal := quaternion
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
-	#if is_rotating:
-		#var interpolated_quat := quaternion.slerp(final_rotation, delta * LOOKAROUND_SPEED)
-		#if interpolated_quat.angle_to(quaternion) < 0.00001:
-			#is_rotating = false
-			#interpolated_quat = final_rotation
-		#quaternion = interpolated_quat
+	var interpolated_quat := quaternion.slerp(goal, delta * LOOKAROUND_SPEED)
+	if interpolated_quat.angle_to(quaternion) > 0.00001:
+		quaternion = interpolated_quat
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -58,10 +38,7 @@ func _physics_process(delta: float) -> void:
 
 func _input(event):
 	if event is InputEventMouseMotion and event.button_mask & 1:
-		var goal := Quaternion(Vector3.MODEL_BOTTOM, event.relative.x) * Quaternion(Vector3.MODEL_LEFT, event.relative.y)
-		var interpolated_quat := quaternion.slerp(goal, LOOKAROUND_SPEED)
-		if interpolated_quat.angle_to(quaternion) > 0.00001:
-			quaternion = interpolated_quat
-		
-		#update_quat()
-		#_on_rotate_pressed()
+		goal = Quaternion(Vector3.MODEL_LEFT, event.relative.y) * Quaternion(Vector3.MODEL_BOTTOM, event.relative.x)
+		goal = goal.normalized()
+	else:
+		goal = quaternion # stop moving without input
