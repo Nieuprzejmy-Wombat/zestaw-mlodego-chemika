716413335e00348270df97d601b1116c2d9f10c2
wombat <u58_makcza_waw@technischools.com>
2025-01-07 23:57:25 +0100

    it rotates! (badly)

--- player.gd
+++ player.gd
@@ -3,8 +3,38 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const LOOKAROUND_SPEED = 0.01
+
+var raw_ijk := Vector3(1, 0, 0)
+var ijk: Vector3
+var raw_w : float
+var quat: Quaternion
+var final_rotation: Quaternion
+var is_rotating := false
+
+func update_quat():
+	if raw_ijk.length() > 0:
+		ijk = raw_ijk.normalized()
+	quat = Quaternion(ijk, deg_to_rad(raw_w))
+	quaternion = Quaternion(Vector3.FORWARD, ijk)
+
+func _on_rotate_pressed():
+	if is_rotating:
+		quaternion = final_rotation
+	is_rotating = true
+	final_rotation = (quat * quaternion).normalized()
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	#if is_rotating:
+		#var interpolated_quat := quaternion.slerp(final_rotation, delta * LOOKAROUND_SPEED)
+		#if interpolated_quat.angle_to(quaternion) < 0.00001:
+			#is_rotating = false
+			#interpolated_quat = final_rotation
+		#quaternion = interpolated_quat
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,8 +43,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# TODO: replace UI actions with custom gameplay actions
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -23,4 +52,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 	move_and_slide()
+	
+
+func _input(event):
+	if event is InputEventMouseMotion and event.button_mask & 1:
+		var goal := Quaternion(Vector3.MODEL_BOTTOM, event.relative.x) * Quaternion(Vector3.MODEL_LEFT, event.relative.y)
+		var interpolated_quat := quaternion.slerp(goal, LOOKAROUND_SPEED)
+		if interpolated_quat.angle_to(quaternion) > 0.00001:
+			quaternion = interpolated_quat
+		
+		#update_quat()
+		#_on_rotate_pressed()
