d80dc4ddd40906c90a07cfb07421592ff577d8ba
wombat <u58_makcza_waw@technischools.com>
2025-01-24 11:29:56 +0100

    fixed glitches when spawning

--- reversal_machine.gd
+++ reversal_machine.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-const DIST := 1000 # TODO: fix: if 100 not spawning properly
+const DIST := 100
 
 @onready var iterations := Time.get_ticks_msec()
 @onready var world: GameWorld = get_tree().current_scene
@@ -14,6 +14,7 @@ func _process(_delta: float) -> void:
 			save.point = -current.point
 			save.shape = current.shape
 			save.position = $Output.global_position
+			save.position.y -= 0.5 # spawn new material under the collision shape so body_entered won't fire again
 			save.rotation = Quaternion.IDENTITY
 			world.spawn_material(save)
 	iterations = new
